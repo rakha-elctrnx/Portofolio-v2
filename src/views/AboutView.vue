80fe3cec3fa6260aa305bf9ee5f1792a87a6728c
Suditama <137113595+Suditama@users.noreply.github.com>
2023-11-25 19:59:56 +0700

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -124,7 +124,7 @@ export default {
         class="text-sm md:text-lg text-justify flex flex-col gap-4 md:flex-row md:gap-8 md:justify-left md:items-center">
         <div class="flex justify-center">
           <img class="w-9/12 rounded-full mb-3 fadein-up"
-            src="https://media.hitekno.com/thumbs/2022/08/25/27609-angie-marcheria-brand-ambassador-bigetron-esports/o-img-27609-angie-marcheria-brand-ambassador-bigetron-esports.jpg" alt="Foto">
+            src="https://i.pinimg.com/564x/71/3d/64/713d6488f6fc17674969f0b55acc79ef.jpg" alt="Foto">
         </div>
         <div class="md:w-7/12">
           <p class="mb-3 md:mb-7 fadein-left fadeins-1">
