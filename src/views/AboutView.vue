a8c3fd22812f11caf17e7b4775d09b9fe9d2a9f7
Suditama <137113595+Suditama@users.noreply.github.com>
2023-11-25 19:21:35 +0700

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -128,7 +128,7 @@ export default {
         </div>
         <div class="md:w-7/12">
           <p class="mb-3 md:mb-7 fadein-left fadeins-1">
-            &nbsp; &nbsp; &nbsp; Hi everyone! My name is Bagas Rakha Putra. I'm a web developer from Bogor, West Java. I
+            &nbsp; &nbsp; &nbsp; Hi everyone! My name is Suditamaa. I'm a web developer from Jember jawa Timur, West Java. I
             have 1 year of experience in back-end web development.
             I really enjoy what I do right now, in my opinion, creating programs is not just a job, but also an art that
             has
