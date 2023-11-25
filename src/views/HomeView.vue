f325ac72d4d7d07ad46d61b5befe26fb77043bb1
Suditama <137113595+Suditama@users.noreply.github.com>
2023-11-25 19:44:56 +0700

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -25,7 +25,7 @@
         </svg>Download Resume</button> -->
     </div>
     <div class="flex justify-center md:justify-start fadein-right"><img alt="avatar" fetchpriority="high" width="300" height="300" decoding="async" data-nimg="1"
-        class="w-10/12 md:h-auto rounded-full border-4 border-amber-200 pict" src="https://media.hitekno.com/thumbs/2022/02/25/57652-angie-marcheria-aka-btr-angie-brand-ambassador-bigetron-esports/730x480-img-57652-angie-marcheria-aka-btr-angie-brand-ambassador-bigetron-esports.jpg">
+        class="w-10/12 md:h-auto rounded-full border-4 border-amber-200 pict" src="anggie1">
     </div>
   </main>
 </template>
