5ebd91f1c3ce09cafecf4a586fe6cc8278f09646
Suditama <137113595+Suditama@users.noreply.github.com>
2023-11-25 20:56:25 +0700

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -40,7 +40,7 @@ const router = createRouter({
 })
 
 router.beforeEach((to, from, next) => {
-  document.title = to.meta.title + ' - bagasRakha();'; // Mengambil judul dari meta atau mengatur judul default jika tidak ada
+  document.title = to.meta.title + ' - Suditama.co.id();'; // Mengambil judul dari meta atau mengatur judul default jika tidak ada
   next();
 });
 
