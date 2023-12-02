3901436a4432782246ffb80fbf174c23d3104c39
Suditama <137113595+Suditama@users.noreply.github.com>
2023-12-03 05:10:33 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -119,7 +119,7 @@ nav a.router-link-exact-active::after {
   width: 100%;
   height: 4px;
   border-radius: 2px;
-  background-color: #ffdb70;
+  background-color: #709bff;
 }
 
 nav a.router-link-exact-active:hover {
