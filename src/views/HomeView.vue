34c78846bbca9a5b9b14f4d80e1a75eab13eb2e4
Suditama <137113595+Suditama@users.noreply.github.com>
2023-12-03 05:33:19 +0700

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,7 +6,7 @@
       <h1 class="text-4xl font-bold md:text-5xl text-white fadein-up">Suditama.</h1>
       <div class="py-2">
         <h1
-          class="typewrite text-xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-slate-100 to-yellow-500 md:text-2xl fadein-up"
+          class="typewrite text-xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-slate-100 to-blue-500 md:text-2xl fadein-up"
           ref="typewriter">
           <span class="wrap">{{ txt }}</span>
         </h1>
