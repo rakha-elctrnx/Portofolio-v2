931fd842116f553dac48f2548a0648ba1666051c
Suditama <137113595+Suditama@users.noreply.github.com>
2023-12-04 05:31:23 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
   <!-- This is an example component -->
   <div class="max-w-7xl mx-auto flex flex-col relative">
 
-    <nav class="max-w-7xl px-5 md:fixed top-0 z-[98] w-screen backdrop-blur-md bg-[#f3f3] bg-opacity-80">
+    <nav class="max-w-7xl px-5 md:fixed top-0 z-[98] w-screen backdrop-blur-md bg-[#121212] bg-opacity-80">
       <div class="container mx-auto flex flex-wrap items-center justify-between">
         <button @click="redirectToHome" class="flex">
           <span class="self-center text-lg text-[#ffdb70] font-semibold whitespace-nowrap fadein-bot hover:text-amber-100">Suditamaa();</span>
@@ -91,7 +91,7 @@ body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #f3f3;
+  color: #2c3e50;
   height: 100vh;
 }
 
