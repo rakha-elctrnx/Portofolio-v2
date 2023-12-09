eba166c3b4e4080b30c4d073ec596d0144a724c7
Suditama <137113595+Suditama@users.noreply.github.com>
2023-12-09 14:08:25 +0700

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -24,7 +24,7 @@ ini<template>
         </svg>Download Resume</button> -->
     </div>
     <div class="flex justify-center md:justify-start fadein-right"><img alt="avatar" fetchpriority="high" width="300" height="300" decoding="async" data-nimg="1"
-        class="w-10/12 md:h-auto rounded-full border-4 border-amber-150 pict" src="https://i.pinimg.com/564x/c6/97/0c/c6970c424dcdf581f48e8e0bd607c427.jpg">
+        class="w-10/12 md:h-auto rounded-full border-4 border-amber-150 pict" src="https://i.pinimg.com/564x/82/89/ed/8289ed8388ee7ef70ef74e9f89001f3a.jpg">
     </div>
   </main>
 </template>
@@ -34,7 +34,7 @@ export default {
   name: 'HomeView',
   data() {
     return {
-      toRotate: ["Youu and Mee", "Full-stack Engineer", "Informatics Student", "Tech Enthusiast"],
+      toRotate: ["You call the shots, babe", "Full-stack Engineer", "Informatics Student", "Tech Enthusiast"],
       period: 2000,
       txt: '',
       loopNum: 0,
