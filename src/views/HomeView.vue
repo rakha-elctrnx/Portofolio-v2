b0d2dc305055d5d347d2a42d212b48a6bca3a20d
Suditama <137113595+Suditama@users.noreply.github.com>
2024-03-03 18:04:32 +0700

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -24,7 +24,7 @@ ini<template>
         </svg>Download Resume</button> -->
     </div>
     <div class="flex justify-center md:justify-start fadein-right"><img alt="avatar" fetchpriority="high" width="300" height="300" decoding="async" data-nimg="1"
-        class="w-10/12 md:h-auto rounded-full border-4 border-amber-150 pict" src="https://pin.it/3rTe48SnC">
+        class="w-10/12 md:h-auto rounded-full border-4 border-amber-150 pict" src="https://pin.it/5iuf2xlzR">
     </div>
   </main>
 </template>
