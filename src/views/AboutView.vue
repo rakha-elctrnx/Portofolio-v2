0cccd34f3077940658367b69554d7942119e2cd8
Suditama <137113595+Suditama@users.noreply.github.com>
2024-03-07 05:47:09 +0700

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -124,7 +124,7 @@ export default {
         class="text-sm md:text-lg text-justify flex flex-col gap-4 md:flex-row md:gap-8 md:justify-left md:items-center">
         <div class="flex justify-center">
           <img class="w-9/12 rounded-full mb-3 fadein-up"
-            src="https://github.com/rakha-elctrnx/Portofolio-v2/pull/19/files#diff-01e3902962792fc967488940ead016aa905cd9a7579bde947f22e10dd8c7428b" alt="Foto">
+            src="https://i.pinimg.com/736x/86/d0/b1/86d0b17dfcac9eb48c4ee994c47fb199.jpg" alt="Foto">
         </div>
         <div class="md:w-7/12">
           <p class="mb-3 md:mb-7 fadein-left fadeins-1">
